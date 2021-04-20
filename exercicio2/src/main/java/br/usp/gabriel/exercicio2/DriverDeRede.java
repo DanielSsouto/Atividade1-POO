3f61d27116b5f425ce6f0a871f9ea26822695b5b
gabrielpassarinho <gabrielpassarelli@hotmail.com>
2021-04-20 14:52:43 -0300

    upei minha pastazinha

---
+++ exercicio2/src/main/java/br/usp/gabriel/exercicio2/DriverDeRede.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.usp.gabriel.exercicio2;
+
+/**
+ *
+ * @author gabriel
+ */
+public class DriverDeRede extends DriverGenerico {
+    //enviaPacoteDeDados
+    DriverDeRede(){
+        super();
+    }
+    
+    public void enviaPacoteDeDados(){
+        //Nao entendi o que esse metodo deveria fazer
+    }
+}
