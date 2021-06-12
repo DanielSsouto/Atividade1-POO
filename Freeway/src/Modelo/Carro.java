547605dde36064094ab98f648a1f098a61ba82aa
gabrielpassarinho <gabrielpassarelli@hotmail.com>
2021-06-12 13:48:30 -0300

    todas as possibilidades de aceleracao/velocidade adicionadas

--- Freeway/src/Modelo/Carro.java
+++ Freeway/src/Modelo/Carro.java
@@ -19,7 +19,7 @@ public class Carro extends Elemento implements Serializable{
     public Carro(String sNomeImagePNG, boolean mortal, String umaCor, int linha, int coluna) {
         super(sNomeImagePNG, linha, coluna);
         cor = umaCor;
-        velocidade_anterior = 1; //menor velocidade possivel
+        velocidade_anterior = (float) 1.5; //menor velocidade possivel
         aceleracao_anterior = 0; // menor aceleracao possivel
         this.bMortal = true;
     }
@@ -28,42 +28,67 @@ public class Carro extends Elemento implements Serializable{
         boolean c = false;
         
         switch(cor){
-                case "azul": // velocidade constante
+            // velocidade constante
+                case "azul": 
                     c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
                                                 this.pPosicao.getColuna() - (int) velocidade_anterior);
                     break;
-                case "amarelo": // aceleracao constante
+                case "vermelho_claro": // 20 por cento mais rapido do que o azul
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                            this.pPosicao.getColuna() - (int) (velocidade_anterior*1.2)); 
+                    break;
+            // aceleracao constante
+                case "amarelo": 
                     c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
                                                 this.pPosicao.getColuna() - (int) velocidade_anterior);
                     if(velocidade_anterior <= 8)
                         velocidade_anterior = velocidade_anterior + aceleracao_anterior;
-                    aceleracao_anterior = (float) 0.5;
+                    aceleracao_anterior = (float) 0.1;
                     break;
-                case "vermelho_claro":
-                    this.setPosicao(10, 10);
+                case "vermelho_esc": // aceleracao 20 por cento maior do que o amarelo
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                                this.pPosicao.getColuna() - (int) velocidade_anterior);
+                    if(velocidade_anterior <= 8)
+                        velocidade_anterior = velocidade_anterior + aceleracao_anterior;
+                    aceleracao_anterior = (float) (0.1*1.2);
                     break;
-                case "vermelho_esc":
-                    this.setPosicao(10, 10);
+            // aceleracao variavel
+                case "verde_claro": //linear
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                                this.pPosicao.getColuna() - (int) velocidade_anterior);
+                    if(velocidade_anterior <= 8)
+                        velocidade_anterior = velocidade_anterior + aceleracao_anterior;
+                    aceleracao_anterior = (float) (this.pPosicao.getColuna()*0.01);
                     break;
-                case "verde_claro": 
-                    this.setPosicao(10, 10);
+                case "verde_esc": //quadratica
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                                this.pPosicao.getColuna() - (int) velocidade_anterior);
+                    if(velocidade_anterior <= 8)
+                        velocidade_anterior = velocidade_anterior + aceleracao_anterior;
+                    aceleracao_anterior = (float) (Math.pow(this.pPosicao.getColuna(),2)*0.01);
                     break;
-                case "verde_esc":
-                    this.setPosicao(10, 10);
-                    break;
-                case "marrom":
-                    this.setPosicao(10, 10);
-                    break;
-                case "rosa":
-                    this.setPosicao(10, 10);
-                    break;
-                case "laranja": // aceleracao senoidal
+            // aceleracao senoidal
+                case "laranja":
                     c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
                                                 this.pPosicao.getColuna() - (int) velocidade_anterior);
                     if (velocidade_anterior <= 8 || aceleracao_anterior <= 0)
                         velocidade_anterior += aceleracao_anterior;
-                    aceleracao_anterior = (float) (1.5*Math.sin(Math.toRadians((191 - this.pPosicao.getColuna())*360/191)));
+                    aceleracao_anterior = (float) (Math.sin((Math.toRadians((191 - this.pPosicao.getColuna())%95)*360/95)));
                     break;
+                case "rosa": // aceleracao 20 por cento maior do que do laranja
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                                this.pPosicao.getColuna() - (int) velocidade_anterior);
+                    if (velocidade_anterior <= 8 || aceleracao_anterior <= 0)
+                        velocidade_anterior += aceleracao_anterior;
+                    aceleracao_anterior = (float) (1.2*Math.sin((Math.toRadians((191 - this.pPosicao.getColuna())%95)*360/95)));
+                    break;
+            // aceleracao aleatoria
+                case "marrom":
+                    c = this.pPosicao.setPosicao(this.pPosicao.getLinha(), 
+                                                this.pPosicao.getColuna() - (int) velocidade_anterior);
+                    if (velocidade_anterior <= 8 || aceleracao_anterior <= 0)
+                        velocidade_anterior += aceleracao_anterior;
+                    aceleracao_anterior = (float) (Math.random()%2+1);
         }
         return c;
     }
@@ -74,40 +99,40 @@ public class Carro extends Elemento implements Serializable{
         
         /*Movo a direita, quando nao poder se mover ainda mais para a direita, pede para ser removido do jogo*/
         if(!this.moveLeft()){
-            /*
+        /*    
             switch (cor){
-                    case "azul": 
-                        //this.setPosicao(10, 185);
-                        break;
                     case "amarelo":
-                        //this.setPosicao(9, 185);
                         velocidade_anterior = 0;
                         aceleracao_anterior = 0;
                         break;
-                    case "vermelho_claro":
-                        //this.setPosicao(10, 10);
-                        break;
                     case "vermelho_esc":
-                        //this.setPosicao(10, 10);
+                        velocidade_anterior = 0;
+                        aceleracao_anterior = 0;
                         break;
-                    case "verde_claro": 
-                        //this.setPosicao(10, 10);
+                    case "verde_claro":
+                        velocidade_anterior = 0;
+                        aceleracao_anterior = 0;
                         break;
                     case "verde_esc":
-                        //this.setPosicao(10, 10);
+                        velocidade_anterior = 0;
+                        aceleracao_anterior = 0;
                         break;
                     case "marrom":
-                        //this.setPosicao(10, 10);
-                        break;
-                    case "rosa":
-                        //this.setPosicao(10, 10);
+                        velocidade_anterior = 0;
+                        aceleracao_anterior = 0;
                         break;
                     case "laranja":
-                        //this.setPosicao(1, 185);
                         velocidade_anterior = 1;
                         aceleracao_anterior = 0;
                         break;
+                    case "rosa":
+                        velocidade_anterior = 1;
+                        aceleracao_anterior = 0;
+                        break;
+                    
             }*/
+            velocidade_anterior = 0;
+            aceleracao_anterior = 0;
             setPosicao(pInicial.getLinha(), pInicial.getColuna());
         }  
             
