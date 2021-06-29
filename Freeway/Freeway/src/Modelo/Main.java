70576cfde3c450a5304e73623f3657a8026e3bc0
DanielSsouto <daniel.souto@usp.com>
2021-06-28 21:30:55 -0300

    deus me ajude

--- Freeway/Freeway/src/Modelo/Main.java
+++ Freeway/Freeway/src/Modelo/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                Tela tTela = new Tela(0);
+                Tela tTela = new Tela(1);
                 tTela.setVisible(true);
                 tTela.createBufferStrategy(2);
                 tTela.go();
